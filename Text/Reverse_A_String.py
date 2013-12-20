9d5d78a623eafefe42efb4bf993558b68c2db9db
GERALD ROYBAL III <geraldmroybal@gmail.com>
2013-12-20 00:37:15 -0500

    Finished Text/Reverse A String
    Completed in 6 lines

--- Text/Reverse_A_String.py
+++ Text/Reverse_A_String.py
@@ -1,9 +1,12 @@
 #**Reverse a String**
 #Enter a string and the program will reverse it and print it out.
 
-originalStr = input("Enter a string to be reversed: ")
+originalStr = ''
+reverseStr = ''
 
-for ch in len(originalStr):
+originalStr = raw_input("Enter a string to be reversed: ")
+
+for ch in range(len(originalStr)):
     reverseStr += originalStr[((len(originalStr) - 1) - ch)]
 
 print reverseStr
